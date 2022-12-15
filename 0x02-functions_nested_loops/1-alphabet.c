5349dba90cd7dc96512f0556811379208d9c26b6
Codemermoz <kouakoumermoz033@gmail.com>
2022-12-15 03:29:44 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * main - print_alphabet
+ * print_alphabet - Entry point
+ *
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
