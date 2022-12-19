30ef43da5b393aa5ad88363edd40271588847410
Codemermoz <kouakoumermoz033@gmail.com>
2022-12-19 04:53:29 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,11 @@
 #include "main.h"
 /**
- * main - entry point
- * Return: Always 0 (success)
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
-void print_alphabet_x10(void)
+int main(void)
 {
-	int j;
-	int x;
-
-	for (x = 0; x < 10; x++)
-	{
-		for (j = 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-		return (0);
-	}
+	print_alphabet_x10();
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * Islower -> checks if the caracter lowercase
+ * Return 1 and 0 depending on condition
+ */
+int _islower(int c)
+{
+	return (c <= 'a' && c <= 'z');
+}
