55da06187dbb34cc518ad842bd424668bce5540a
Codemermoz <kouakoumermoz033@gmail.com>
2022-12-28 04:34:06 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (str[i] >= 'a' && str[i] <= 'z')
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
@@ -26,7 +26,7 @@ char *cap_string(char *str)
 		str[i - 1] == '{' ||
 		str[i - 1] == '}' ||
 		i == 0)
-		str[i] -= 32;
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
